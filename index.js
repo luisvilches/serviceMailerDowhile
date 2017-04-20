cc9e80e066887f28209c18cba280bb3d8381c069
LuisVilches <lvilches21@gmail.com>
2017-04-20 20:46:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const NodeMailer = require('nodemailer');
 const fs = require('fs')
 
 let app = express();
-
+let port = proccess.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -63,4 +63,4 @@ app.get('/test', (req,res) => {
     fs.appendFileSync('message.js', `Router.post('/app', controller.dowhile.dowhile); \n`);
 })
 
-app.listen(3000);
+app.listen(port);
