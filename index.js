e260184398bc8ebdf0005a3c4e59d4c72b1ab7a4
Luis Vilches <luis.vilches@cardumen.cl>
2018-09-12 15:57:04 -0300

    change email

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('connect-multiparty')();
 const NodeMailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
+const Emailer = require('google-zoho-node-mailer');
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5005;
@@ -27,11 +28,22 @@ app.post('/send',bodyParser, (req,res) => {
 
 
     let mailOptions = {
-        from: remitente,
-        to: 'lvilches21@gmail.com',
+        from: remitente + "<" + remitente + ">",
+        to: 'luis@valadigital.cl',
         subject: 'Formulario de contacto web',
         html: template
     };
+    /*
+    Emailer.UseZohoSMTPTransport({
+        username: 'luis@valadigital.cl',
+        password: 'andres3190'
+    });
+
+    var message = new Emailer.Email(mailOptions)
+     
+    message.send(function(status){
+        console.log(status);
+    })*/
 
     /*let transporter = NodeMailer.createTransport(smtpTransport({
         service: 'gmail',
@@ -41,15 +53,47 @@ app.post('/send',bodyParser, (req,res) => {
             user: 'lvilches21@gmail.com',
             pass: 'andres3190'
         }
-    }));*/
+    }));
 
-    var transporter = NodeMailer.createTransport({
-        service: "gmail",
+    let smtpConfig = {
+        pool:true,
+        host: 'smtp.zoho.eu',
+        port: 465,
+        secure: true, // upgrade later with STARTTLS,
+        requireTLS: true,
+        //authMethod:'AUTH PLAIN',
         auth: {
-        user: "lvilches21@gmail.com", // Cambialo por tu email
-        pass: "andres3190" // Cambialo por tu password
+            user: 'luis@valadigital.cl',
+            pass: 'andres3190'
         }
-    });
+        host: 'smtp.zoho.com',
+        port: 587,
+        //secure: true, // use SSL
+        //tls: {rejectUnauthorized: true},
+        debug:true,
+        //authMethod:'SSL',
+        auth: {
+            user: 'hola@valadigital.cl',
+            pass: 'andres2230'
+        }
+    };*/
+    //var transporter = NodeMailer.createTransport('SMTP',smtpConfig);
+    //var transporter = NodeMailer.createTransport("SMTP", smtpConfig);
+    /*var transporter = NodeMailer.createTransport("SMTP",{
+        service: "Zoho",
+        auth: {
+            user: "hola@valadigital.cl", // Cambialo por tu email
+            pass: "andres2230" // Cambialo por tu password
+        }
+    });*/
+
+    let transporter = NodeMailer.createTransport(smtpTransport({
+        service: "Gmail",
+        auth: {
+            user: "lvilches21@gmail.com", // Cambialo por tu email
+            pass: "andres3190" // Cambialo por tu password
+        }
+    }));
 
     transporter.sendMail(mailOptions, function(error, info){
         if (error){
@@ -65,5 +109,5 @@ app.post('/send',bodyParser, (req,res) => {
 
 app.listen(port, function(err){
     if(err) throw err;
-    console.log('server running in port 5000');
+    console.log('server running in port ' + port);
 });
